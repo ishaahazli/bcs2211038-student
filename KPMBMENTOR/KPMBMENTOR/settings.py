b220a8376fdd753773546684aecc6120d1be6554
ishaahazli <160446371+ishaahazli@users.noreply.github.com>
2024-03-04 10:17:26 +0800

    Update settings.py

--- KPMBMENTOR/KPMBMENTOR/settings.py
+++ KPMBMENTOR/KPMBMENTOR/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-%p05nsy!bpfwf5-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG','')!='False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [' ishaahazli.pythonanywhere.com']
 
 
 # Application definition
